70c728dd5f80d753bc33e5522a30d69ae779a1d1
Victor KVV <kvv_post@mail.ru>
2023-08-05 00:27:23 +0300

    Games of mind 5.1

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greating();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.print("1 - Greet ");
+        System.out.print("0 - Exit ");
+        System.out.print("Your choice: ");
+        String choice = scanner.next();
+        if (choice.equals("1")) {
+            Cli.greating();
+        }
+        scanner.close();
+//        Cli.greating();
     }
 }
