a87ffd7dc379af1aca5683394487746e9c39a8aa
Victor kvv <kvv_post@mail.ru>
2023-08-08 18:36:56 +0300

    end of step 5

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,15 +6,15 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
-        System.out.print("1 - Greet ");
-        System.out.print("0 - Exit ");
-        System.out.print("Your choice: ");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.println("Your choice: ");
         String choice = scanner.next();
         if (choice.equals("1")) {
             Cli.greating();
         } else if (choice.equals("2")) {
             randomGame.rG();
-
         }
         scanner.close();
 //        Cli.greating();
--- app/src/main/java/hexlet/code/randomGame.java
+++ app/src/main/java/hexlet/code/randomGame.java
@@ -1,22 +1,37 @@
 package hexlet.code;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class randomGame {
     public static void rG() {
-        int rNumber = (int)Math.random();
-        String isEven = (rNumber % 2 == 0)  ? "yes" : "no";
+        int rNumber;
+        int i;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        System.out.println("Question: " + rNumber);
-        System.out.print("Your answer: ");
-        String answer = scanner.next();
-        if (isEven.equals(answer)) {
-
+        for (i = 0; i < 3; i++) {
+            rNumber = (int)(Math.random()*10);
+            Random r = new Random();
+            rNumber = r.nextInt(100) + 1;
+            String isEven = (rNumber % 2 == 0)  ? "yes" : "no";
+            System.out.println("Question: " + rNumber);
+            System.out.print("Your answer: ");
+            String answer = scanner.next();
+            if (isEven.equals(answer)) {
+                System.out.println("Correct!");
+            }
+            else {
+                System.out.println("'"+ answer + "' is wrong answer ;(. Correct answer was '"+ isEven +"'.");
+                System.out.println("Let's try again, " + userName + "!");
+                break;
+            }
+        }
+        if (i >= 2) {
+            System.out.println("Congratulations, " + userName + "!");
         }
     }
 }
