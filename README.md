ab06f9ad32e5780c5c94848fccaa8f131731278e
Victor KVV <kvv_post@mail.ru>
2023-09-04 21:54:32 +0300

    new asciinema

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/VictorKVV-hex/java-project-61/workflows/hexlet-check/badge.svg)](https://github.com/VictorKVV-hex/java-project-61/actions)
 
-https://asciinema.org/a/pIp05EBJVWcBeiHn59Ejniohr
+https://asciinema.org/a/lb4EiVOmvnPPt4p0J73gqd7vI
 
-https://asciinema.org/a/U0mvEGNogr5ugcxJ12dlxOV7L
+https://asciinema.org/a/otLcEN25raeMF9yedeQPGdc1s
 
-https://asciinema.org/a/2hn6VwTjQvIxcwszTYiIJqqvl
+https://asciinema.org/a/S1KSW19P9nZhkuHkdrQCYmuO7
 
-https://asciinema.org/a/FVHcpIrHRSqeWNpuqgpSuFoHN
+https://asciinema.org/a/d7IMNHSPnopPweiGvBPPXvldT
 
-https://asciinema.org/a/aYpQI1zmmxeg9Fmlq0DDjneLU
+https://asciinema.org/a/a3YB2zRnByBs6tJ6Kbe8q8aTu
--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,12 +4,6 @@ import java.util.Scanner;
 
 public class Engine {
 
-    static final int GAME_EVEN = 2;
-    static final int GAME_CALC = 3;
-    static final int GAME_GCD = 4;
-    static final int GAME_PROGRESSION = 5;
-    static final int GAME_PRIME = 6;
-
     public static String getUserName() {
         return userName;
     }
@@ -20,15 +14,14 @@ public class Engine {
 
     private static String userName;
 
-    public static void goGame(String ruls, String[][] rezGame) {
+    public static void goGame(String rule, String[][] rezGame) {
         int i = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         Engine.setUserName(scanner.next());
         System.out.println("Hello, " + getUserName() + "!");
-//        System.out.println(Engine.getStrMainQuestion(numGame));
-        System.out.println(ruls);
+        System.out.println(rule);
         final int countOfGame = 3;
         for (i = 0; i < countOfGame; i++) {
             String sQuestion = rezGame[i][0];
@@ -48,31 +41,5 @@ public class Engine {
             System.out.println("Congratulations, " + userName + "!");
         }
     }
-
-
-
-//    public static String getStrMainQuestion(int numGame) {
-//        String question = "";
-//        switch (numGame) {
-//            case  GAME_EVEN:
-//                question = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-//                break;
-//            case  GAME_CALC:
-//                question = "What is the result of the expression?";
-//                break;
-//            case  GAME_GCD:
-//                question = "Find the greatest common divisor of given numbers.";
-//                break;
-//            case  GAME_PROGRESSION:
-//                question = "What number is missing in the progression?";
-//                break;
-//            case  GAME_PRIME:
-//                question = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-//                break;
-//            default:
-//                break;
-//        }
-//        return question;
-//    }
 
 }
